ff244f3f370dbd36744eb52081076f291b0ebcbd
Mirock <kashingeotgii@gmail.com>
2026-04-06 16:57:38 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,8 +1,140 @@
-// Copyright 2022 GHA Test Team
-
+// Copyright 2026 KashinGS
 #include <gtest/gtest.h>
 #include "Automata.h"
-TEST(AutomataTest, InitialStateIsOff) {
+
+TEST(AutomataTest, Test1_InitialStateIsOff) {
     Automata a;
     EXPECT_EQ(a.getState(), STATES::OFF);
+}
+
+TEST(AutomataTest, Test2_OnTransitionsToWait) {
+    Automata a;
+    a.on();
+    EXPECT_EQ(a.getState(), STATES::WAIT);
+}
+
+TEST(AutomataTest, Test3_FirstCoinTransitionsToAccept) {
+    Automata a;
+    a.on();
+    a.coin(10);
+    EXPECT_EQ(a.getState(), STATES::ACCEPT);
+}
+
+TEST(AutomataTest, Test4_SubsequentCoinStaysInAccept) {
+    Automata a;
+    a.on();
+    a.coin(10);
+    a.coin(20);
+    EXPECT_EQ(a.getState(), STATES::ACCEPT);
+}
+
+TEST(AutomataTest, Test5_ChoiceTransitionsToCheck) {
+    Automata a;
+    a.on();
+    a.coin(50);
+    a.choice(1);
+    EXPECT_EQ(a.getState(), STATES::CHECK);
+}
+
+TEST(AutomataTest, Test6_CheckReturnsTrueWhenEnoughMoney) {
+    Automata a;
+    a.on();
+    a.coin(50);
+    a.choice(3);
+    EXPECT_TRUE(a.check());
+}
+
+TEST(AutomataTest, Test7_CheckReturnsFalseWhenNotEnoughMoney) {
+    Automata a;
+    a.on();
+    a.coin(20);
+    a.choice(4);
+    EXPECT_FALSE(a.check());
+}
+
+TEST(AutomataTest, Test8_CookTransitionsToCook) {
+    Automata a;
+    a.on();
+    a.coin(50);
+    a.choice(2);
+    a.cook();
+    EXPECT_EQ(a.getState(), STATES::COOK);
+}
+
+TEST(AutomataTest, Test9_FinishTransitionsToWait) {
+    Automata a;
+    a.on();
+    a.coin(50);
+    a.choice(2);
+    a.cook();
+    a.finish();
+    EXPECT_EQ(a.getState(), STATES::WAIT);
+}
+
+TEST(AutomataTest, Test10_CancelFromAcceptReturnsMoneyAndGoesToWait) {
+    Automata a;
+    a.on();
+    a.coin(100);
+    a.cancel();
+    EXPECT_EQ(a.getState(), STATES::WAIT);
+    a.coin(10);
+    a.choice(4);
+    EXPECT_FALSE(a.check());
+}
+
+TEST(AutomataTest, Test11_CancelFromCheckGoesToWait) {
+    Automata a;
+    a.on();
+    a.coin(30);
+    a.choice(1);
+    a.cancel();
+    EXPECT_EQ(a.getState(), STATES::WAIT);
+}
+
+TEST(AutomataTest, Test12_ChoiceFailsInWait) {
+    Automata a;
+    a.on();
+    EXPECT_FALSE(a.choice(1));
+    EXPECT_EQ(a.getState(), STATES::WAIT);
+}
+
+TEST(AutomataTest, Test13_OffWorksOnlyFromWait) {
+    Automata a;
+    a.on();
+    a.off();
+    EXPECT_EQ(a.getState(), STATES::OFF);
+    a.off();
+    EXPECT_EQ(a.getState(), STATES::OFF);
+}
+
+TEST(AutomataTest, Test14_CookFailsWhenNotEnoughMoney) {
+    Automata a;
+    a.on();
+    a.coin(20);
+    a.choice(3);
+    a.cook();
+    EXPECT_EQ(a.getState(), STATES::CHECK);
+}
+
+TEST(AutomataTest, Test15_MoneyIsDebitedAfterSuccessfulPurchase) {
+    Automata a;
+    a.on();
+    a.coin(100);
+    a.choice(5);
+    EXPECT_TRUE(a.check());
+    a.cook();
+    a.finish();
+    a.coin(0);
+    a.choice(1);
+    EXPECT_TRUE(a.check());
+}
+
+TEST(AutomataTest, Test16_CancelWithNoMoneyDoesNothing) {
+    Automata a;
+    a.on();
+    a.cancel();
+    EXPECT_EQ(a.getState(), STATES::WAIT);
+    a.coin(5);
+    a.choice(1);
+    EXPECT_FALSE(a.check());
 }
