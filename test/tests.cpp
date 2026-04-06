2c084ccf2960b77977eab1d95a2a073fd1f3ff08
Mirock <kashingeotgii@gmail.com>
2026-04-06 16:24:47 +0300

    Update all files

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,4 +1,7 @@
 // Copyright 2022 GHA Test Team
-
 #include <gtest/gtest.h>
 #include "Automata.h"
+TEST(AutomataTest, InitialStateIsOff) {
+    Automata a;
+    EXPECT_EQ(a.getState(), STATES::OFF);
+}
