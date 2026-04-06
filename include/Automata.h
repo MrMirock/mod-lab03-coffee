f120f7a1d1417484c9f81783dd52abf90087b4ab
Mirock <kashingeotgii@gmail.com>
2026-04-06 16:21:06 +0300

    Add files via upload

---
+++ include/Automata.h
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+
+enum class STATES 
+{
+    OFF,
+    WAIT, 
+    ACCEPT,
+    CHECK,
+    COOK
+};
+
+class Automata 
+{
+private:
+    int cash;
+    std::vector<std::string> menu;
+    std::vector<int> prices;
+    STATES state;
+    int selectedPrice;
+
+public:
+    Automata();
+
+    void on();
+    void off();
+    void coin(int amount);
+    std::string getMenu();
+    STATES getState();
+    bool choice(int index);
+    bool check();
+    void cancel();
+    void cook();
+    void finish();
+};
